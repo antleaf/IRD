58a086eeff9511902e3b5a845f48bd6938feaf17
paulwalk <paul@paulwalk.net>
2025-07-09 14:32:16 +0100

    fixed autocurate to respect manual override of systems checks

--- app/services/curation/system_candidate_defunct_checker_service.rb
+++ app/services/curation/system_candidate_defunct_checker_service.rb
@@ -3,19 +3,28 @@ module Curation
   class SystemCandidateDefunctCheckerService < ApplicationService
     def call(system)
       begin
-        nc_homepage = system.network_checks.homepage_url_failed.first
         nc_oai = system.network_checks.oai_pmh_identify_failed.first
-        if nc_homepage&.errors_past_threshold? && nc_oai&.errors_past_threshold?
-          system.label_list.add("candidate-defunct")
-          system.set_record_under_review! if system.record_status_verified?
-        else
+        if system.system_status_online?
           system.label_list.remove("candidate-defunct")
-        end
-        if nc_oai&.errors_past_threshold?
-          system.label_list.add("candidate-out-of-scope")
-          system.set_record_under_review! if system.record_status_verified?
         else
+          nc_homepage = system.network_checks.homepage_url_failed.first
+          if nc_homepage&.errors_past_threshold? && nc_oai&.errors_past_threshold?
+            system.label_list.add("candidate-defunct")
+            system.set_record_under_review! if system.record_status_verified?
+          else
+            system.label_list.remove("candidate-defunct")
+          end
+        end
+
+        if system.oai_status_online?
           system.label_list.remove("candidate-out-of-scope")
+        else
+          if nc_oai&.errors_past_threshold?
+            system.label_list.add("candidate-out-of-scope")
+            system.set_record_under_review! if system.record_status_verified?
+          else
+            system.label_list.remove("candidate-out-of-scope")
+          end
         end
       end
       success system
