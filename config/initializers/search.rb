f23d71969269f15e81be963bba5563e4f0e42a1f
Sebastian Smith <horizon_mode@outlook.com>
2025-09-23 17:46:32 +0100

    stop SearchKick verifying certs

--- config/initializers/search.rb
+++ config/initializers/search.rb
@@ -1,3 +1,11 @@
 Searchkick.model_options = {
   batch_size: ENV.fetch('OPENSEARCH_BATCH_SIZE',1000).to_i
 }
+
+Searchkick.client_options = {
+    transport_options: {
+      ssl: {
+        verify: false,
+      }
+    }
+  }
