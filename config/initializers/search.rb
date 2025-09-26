e4176649463a31d857e011d760d0f5eedaedf10d
Sebastian Smith <horizon_mode@outlook.com>
2025-09-26 12:00:50 +0100

    add certs to path for opensearch

--- config/initializers/search.rb
+++ config/initializers/search.rb
@@ -1,11 +1,13 @@
 Searchkick.model_options = {
-  batch_size: ENV.fetch('OPENSEARCH_BATCH_SIZE',1000).to_i
+  batch_size: ENV.fetch("OPENSEARCH_BATCH_SIZE", 1000).to_i
 }
-
-Searchkick.client_options = {
+if ENV["OPENSEARCH_CA_CERT_PATH"].present? && File.exist?(ENV["OPENSEARCH_CA_CERT_PATH"])
+  Searchkick.client_options = {
     transport_options: {
       ssl: {
         verify: false,
+        ca_file: ENV["OPENSEARCH_CA_CERT_PATH"]
       }
     }
   }
+end
