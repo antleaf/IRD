ea21473ee6567428e597cba653ce15a9a0666114
Paul Walk <paul@paulwalk.net>
2026-09-02 15:41:37 +0100

    modified ror ingest

--- Gemfile
+++ Gemfile
@@ -91,6 +91,7 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
   gem "i18n-tasks", "~> 1.1.2"
+  gem "solargraph"
 end
 
 group :test do
--- app/services/ingest/ror_ingest_batch_service.rb
+++ app/services/ingest/ror_ingest_batch_service.rb
@@ -2,45 +2,44 @@
 
 module Ingest
   class RorIngestBatchService < ApplicationService
-    require 'json'
+    require "json"
 
     def call(data_file_path)
       begin
-        updated_count = 0
         created_count = 0
         error_count = 0
         data = JSON.parse(File.read(data_file_path))
         data.each do |record|
           begin
             record_hash = {}
-            record_hash[:id] = record['id']
-            locations = record['locations']
+            record_hash[:id] = record["id"]
+            locations = record["locations"]
             if locations && locations.count > 0
-              geonames_details = locations[0]['geonames_details']
+              geonames_details = locations[0]["geonames_details"]
               if geonames_details
-                record_hash[:country_code] = geonames_details['country_code']
-                record_hash[:location] = geonames_details['name']
-                record_hash[:latitude] = geonames_details['lat']
-                record_hash[:longitude] = geonames_details['lng']
+                record_hash[:country_code] = geonames_details["country_code"]
+                record_hash[:location] = geonames_details["name"]
+                record_hash[:latitude] = geonames_details["lat"]
+                record_hash[:longitude] = geonames_details["lng"]
               end
             end
-            links = record['links']
+            links = record["links"]
             if links && links.count > 0
               links.each do |link|
-                if link['type'] == 'website'
-                  record_hash[:website] = link['value']
+                if link["type"] == "website"
+                  record_hash[:website] = link["value"]
                   break
                 end
               end
             end
             record_hash[:aliases] = []
-            names = record['names']
+            names = record["names"]
             if names && names.count > 0
               names.each do |name|
-                if name['types'].include? 'ror_display'
-                  record_hash[:name] = name['value']
+                if name["types"].include? "ror_display"
+                  record_hash[:name] = name["value"]
                 else
-                  record_hash[:aliases] << name['value']
+                  record_hash[:aliases] << name["value"]
                 end
               end
             end
@@ -55,11 +54,7 @@ module Ingest
               website: record_hash[:website]
             }
             org = Organisation.find_by(ror: record_hash[:id])
-            if org
-              org.update(attributes)
-              updated_count += 1
-              Rails.logger.debug "Updated Organisation with ROR ID: #{org.ror} OK"
-            else
+            if !org
               org = Organisation.create(attributes)
               created_count += 1
               Rails.logger.info "Created Organisation with ROR ID: #{org.ror}..."
@@ -72,7 +67,6 @@ module Ingest
       end
       Rails.logger.info "Processed #{data.count} records"
       Rails.logger.info "#{created_count} records created"
-      Rails.logger.info "#{updated_count} records updated"
       Rails.logger.info "#{error_count} errors"
       success true
     rescue Exception => e2
@@ -80,4 +74,84 @@ module Ingest
       failure e2
     end
   end
+
+  # class RorIngestBatchService < ApplicationService
+  #   require 'json'
+  #
+  #   def call(data_file_path)
+  #     begin
+  #       updated_count = 0
+  #       created_count = 0
+  #       error_count = 0
+  #       data = JSON.parse(File.read(data_file_path))
+  #       data.each do |record|
+  #         begin
+  #           record_hash = {}
+  #           record_hash[:id] = record['id']
+  #           locations = record['locations']
+  #           if locations && locations.count > 0
+  #             geonames_details = locations[0]['geonames_details']
+  #             if geonames_details
+  #               record_hash[:country_code] = geonames_details['country_code']
+  #               record_hash[:location] = geonames_details['name']
+  #               record_hash[:latitude] = geonames_details['lat']
+  #               record_hash[:longitude] = geonames_details['lng']
+  #             end
+  #           end
+  #           links = record['links']
+  #           if links && links.count > 0
+  #             links.each do |link|
+  #               if link['type'] == 'website'
+  #                 record_hash[:website] = link['value']
+  #                 break
+  #               end
+  #             end
+  #           end
+  #           record_hash[:aliases] = []
+  #           names = record['names']
+  #           if names && names.count > 0
+  #             names.each do |name|
+  #               if name['types'].include? 'ror_display'
+  #                 record_hash[:name] = name['value']
+  #               else
+  #                 record_hash[:aliases] << name['value']
+  #               end
+  #             end
+  #           end
+  #           attributes = {
+  #             ror: record_hash[:id],
+  #             name: record_hash[:name],
+  #             aliases: record_hash[:aliases],
+  #             country: Country.find(record_hash[:country_code]),
+  #             location: record_hash[:location],
+  #             latitude: record_hash[:latitude].to_f,
+  #             longitude: record_hash[:longitude].to_f,
+  #             website: record_hash[:website]
+  #           }
+  #           org = Organisation.find_by(ror: record_hash[:id])
+  #           if org
+  #             org.update(attributes)
+  #             updated_count += 1
+  #             Rails.logger.debug "Updated Organisation with ROR ID: #{org.ror} OK"
+  #           else
+  #             org = Organisation.create(attributes)
+  #             created_count += 1
+  #             Rails.logger.info "Created Organisation with ROR ID: #{org.ror}..."
+  #           end
+  #         rescue Exception => e
+  #           Rails.logger.warn("Error processing #{record['id']}: #{e.message}")
+  #           error_count += 1
+  #         end
+  #       end
+  #     end
+  #     Rails.logger.info "Processed #{data.count} records"
+  #     Rails.logger.info "#{created_count} records created"
+  #     Rails.logger.info "#{updated_count} records updated"
+  #     Rails.logger.info "#{error_count} errors"
+  #     success true
+  #   rescue Exception => e2
+  #     Rails.logger.error e2.message
+  #     failure e2
+  #   end
+  # end
 end
